2079e14a85249e1751d64c975e155cd9e24cc325
raplisopan-cell <raplisopan@gmail.com>
2026-09-16 14:56:39 +0700

    tambah

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-{
-  "name": "pkl-modern-js-library",
-  "version": "1.0.0",
-  "type": "module",
-  "description": "Program perpustakaan menggunakan Node.js modular",
-  "scripts": {
-    "start": "node index.js"
-  }
-}
+console.log("Program Perpustakaan Berjalan");
