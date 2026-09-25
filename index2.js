cc55a0053ba725e8658a1d0de310050f54368582
raplisopan-cell <raplisopan@gmail.com>
2026-09-25 15:59:31 +0700

    hapus file tugas lama

--- index2.js
+++
@@ -1,9 +0,0 @@
-const { buku, peminjaman } = require('./data');
-
-peminjaman.forEach(p => {
-    const b = buku.find(b => b.id === p.bukuId);
-
-    console.log(
-        `${p.namaPeminjam} meminjam ${b.judul} - ${p.status}`
-    );
-});
--- src/testJson.js
+++
@@ -1,6 +0,0 @@
-import { readJson } from "./repositories/jsonRepository.js";
-
-const books = await readJson("./data/books.json");
-
-console.log("Jumlah buku:", books.length);
-console.log("Buku pertama:", books[0].title);
